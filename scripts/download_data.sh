9a442a4ba08b54f598ca51b7074f76c034e03c44
akanametov <akkanametov@gmail.com>
2023-01-06 09:53:06 +0000

    update download data script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 echo "Donwloading dataset ..."
 
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=0B8okgV6zu3CCTk96SW9IWFJ6RE0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0B8okgV6zu3CCTk96SW9IWFJ6RE0" -O data/landmarks_task.tgz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=${1:-0B8okgV6zu3CCWlU3b3p4bmJSVUU}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${1:-0B8okgV6zu3CCWlU3b3p4bmJSVUU}" -O data/landmarks_task.tgz && rm -rf /tmp/cookies.txt
 
 echo "Unziping dataset ..."
 
-cd data;
-unzip landmarks_task.tgz;
+cd data
+tar -xvzf landmarks_task.tgz
