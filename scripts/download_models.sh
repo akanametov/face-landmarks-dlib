ccd99bd5e53e949167bd63c7e80d7069b77c72f5
akanametov <akkanametov@gmail.com>
2023-01-18 06:35:20 +0000

    add dlib model download script

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -5,3 +5,5 @@ wget "https://github.com/akanametov/face-landmarks-dlib/releases/download/0.9/sh
 wget "https://github.com/akanametov/face-landmarks-dlib/releases/download/0.9/shape_predictor_68_Menpo.dat" -O output/shape_predictor_68_Menpo.dat 
 
 wget "https://github.com/akanametov/face-landmarks-dlib/releases/download/0.9/shape_predictor_68_300W_Menpo.dat" -O output/shape_predictor_68_300W_Menpo.dat 
+
+wget "https://github.com/akanametov/face-landmarks-dlib/releases/download/0.9/shape_predictor_68_face_landmarks.dat" -O output/shape_predictor_68_face_landmarks.dat 
